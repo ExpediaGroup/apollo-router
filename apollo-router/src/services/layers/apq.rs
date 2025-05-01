ee80c52d9eccd5a87286a1167852ac721e790ad3
Jorge Aguilar <59518622+Aguilarjaf@users.noreply.github.com>
2025-05-01 12:52:37 -0700

    feat: log operation name for PersistedQueryNotFound events (#24)
    * feat: log operation name for PersistedQueryNotFound events
    
    * remove format macro as operation_name is a string type

--- apollo-router/src/services/layers/apq.rs
+++ apollo-router/src/services/layers/apq.rs
@@ -164,6 +164,13 @@ async fn apq_request(
             } else {
                 let _ = request.context.insert(PERSISTED_QUERY_CACHE_HIT, false);
                 tracing::trace!("apq: cache miss");
+
+                let operation_name = request.supergraph_request.body().operation_name
+                    .clone()
+                    .unwrap_or_else(|| "NO OPERATION NAME PROVIDED".to_string());
+                
+                tracing::info!(message = "PersistedQueryNotFound", operation = operation_name);
+                
                 let errors = vec![crate::error::Error {
                     message: "PersistedQueryNotFound".to_string(),
                     locations: Default::default(),

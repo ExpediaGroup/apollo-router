c8a1e95862a4e078c541b1f24ff255b6de8c63b5
Michelle Fast <97126970+MichelleFast8@users.noreply.github.com>
2025-11-18 15:35:40 -0800

    feat: log query plan operation duration (#36)
    * Move log, access name via different variable, log duration
    
    * Compute elapsed time only once
    
    * Type mismatch: change to String
    
    * Rename duration to duration_ms
    
    ---------
    
    Co-authored-by: Michelle Fast <mfast@expediagroup.com>

--- apollo-router/src/query_planner/query_planner_service.rs
+++ apollo-router/src/query_planner/query_planner_service.rs
@@ -171,10 +171,6 @@ impl QueryPlannerService {
                 .map(|n| Name::new(n).map_err(FederationError::from))
                 .transpose()
                 .and_then(|operation| {
-                    tracing::info!({
-                        message = "Building query plan",
-                        operation = format!("{}", operation.clone().unwrap_or(Name::new_static_unchecked("NO OPERATION NAME PROVIDED"))),
-                    });
                     rust_planner.build_query_plan(&doc.executable, operation, query_plan_options)
                 });
             if let Err(FederationError::SingleFederationError(
@@ -464,10 +460,16 @@ impl Service<QueryPlannerRequest> for QueryPlannerService {
                 )
                 .await;
 
+            let duration = start.elapsed();
+            tracing::info!({
+                message = "Building query plan",
+                operation = format!("{}", operation_name.clone().unwrap_or(String::from("NO OPERATION NAME PROVIDED"))),
+                duration_ms = %duration.as_millis(),
+            });
             f64_histogram!(
                 "apollo.router.query_planning.total.duration",
                 "Duration of the time the router waited for a query plan, including both the queue time and planning time, in seconds.",
-                start.elapsed().as_secs_f64()
+                duration.as_secs_f64()
             );
 
             match res {

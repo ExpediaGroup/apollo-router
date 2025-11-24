6b3b93ffe588a4b388349623fdc65700ad14cabe
Samuel Vazquez <samvazquez@expediagroup.com>
2025-11-24 09:09:19 -0800

    feat: keep DeduplicatingCache unchanged

--- apollo-router/src/cache/mod.rs
+++ apollo-router/src/cache/mod.rs
@@ -222,19 +222,6 @@ where
         }
     }
 
-    pub(crate) async fn insert_in_memory(self, value: V) {
-        if let EntryInner::First {
-            key,
-            sender,
-            cache,
-            ..
-        } = self.inner
-        {
-            cache.insert_in_memory(key.clone(), value.clone()).await;
-            cache.send(sender, &key, Ok(value)).await;
-        }
-    }
-
     /// sends the value without storing it into the cache
     #[allow(unused)]
     pub(crate) async fn send(self, value: Result<V, UncachedError>) {

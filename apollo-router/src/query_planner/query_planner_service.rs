2517b5ecf3d1c94da6928b93b891976c8f19ff5a
Samuel Vazquez <sam_2f@hotmail.com>
2025-12-01 16:04:19 -0800

    feat: min query planning duration to store in distributed cache (#37)
    * feat: min query planning duration to store in distributed cache
    
    * feat: keep DeduplicatingCache unchanged
    
    * feat: remove unnecessary import
    
    * feat: keep QueryPlannerContent unchanged
    
    ---------
    
    Co-authored-by: Samuel Vazquez <samvazquez@expediagroup.com>

--- apollo-router/src/query_planner/query_planner_service.rs
+++ apollo-router/src/query_planner/query_planner_service.rs
@@ -209,6 +209,7 @@ impl QueryPlannerService {
         }
 
         Ok(QueryPlanResult {
+            // tempted to get rid of this, it might cause CPU usage to go up for no good reason
             formatted_query_plan: Some(Arc::new(plan.to_string())),
             query_plan_root_node: root_node.map(Arc::new),
             evaluated_plan_count: plan.statistics.evaluated_plan_count.clone().into_inner() as u64,
@@ -318,6 +319,7 @@ impl QueryPlannerService {
         compute_job_type: ComputeJobType,
         query_metrics: OperationLimits<u32>,
     ) -> Result<QueryPlannerContent, MaybeBackPressureError<QueryPlannerError>> {
+        let start = Instant::now();
         let plan_result = self
             .plan_inner(
                 doc,
@@ -360,6 +362,8 @@ impl QueryPlannerService {
             &self.signature_normalization_algorithm,
         );
 
+        let elapsed = start.elapsed();
+
         if let Some(node) = query_plan_root_node {
             u64_histogram!(
                 "apollo.router.query_planning.plan.evaluated_plans",
@@ -380,6 +384,7 @@ impl QueryPlannerService {
                     query: Arc::new(selections),
                     query_metrics,
                     estimated_size: Default::default(),
+                    compute_duration: Some(elapsed),
                 }),
             })
         } else {
